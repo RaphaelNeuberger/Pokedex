918764b6bcd736ea23bec6b19a44b344f51abc81
Raphael Neuberger <info@raphael-neuberger.com>
2025-10-19 12:01:23 +0200

    heart update in script

--- script.js
+++ script.js
@@ -134,6 +134,18 @@ function getPokemonCardHTML(pokemon) {
 
   return `
     <div class="pokemon-id">#${formatPokemonId(pokemon.id)}</div>
+    <button class="favorite-button" id="favoriteButton">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2">
+              <path
+                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+            </svg>
+          </button>
     <img src="${pokemon.sprites.other["official-artwork"].front_default}" 
          alt="${pokemon.name}" 
          class="pokemon-image">
